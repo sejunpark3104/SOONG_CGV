492e7e35fe08dc564cbaad3f1f002c06bbd2e0e4
sejunpark3104 <wound3004@gmail.com>
2017-12-08 17:24:08 +0900

    Delete test.c

--- mymovie/src/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "movie.h"
-
-int test(Context* context, int cmdCnt, char* cmdArg[]) {
-	int str[10] = {1,};
-	printf("Just for test %d %d\n", str[0], str[1]);
-	return 0;
-}
